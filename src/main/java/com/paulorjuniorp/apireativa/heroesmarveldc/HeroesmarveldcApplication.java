7d38b24047d849397e7d14110fb805eab17fbd1e
paulorjuniorp <paulorjuniorp@gmail.com>
2021-12-06 22:41:00 -0300

    Primeiro commit
    Adição dos arquivos no github

---
+++ src/main/java/com/paulorjuniorp/apireativa/heroesmarveldc/HeroesmarveldcApplication.java
@@ -0,0 +1,13 @@
+package com.paulorjuniorp.apireativa.heroesmarveldc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroesmarveldcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroesmarveldcApplication.class, args);
+	}
+
+}
